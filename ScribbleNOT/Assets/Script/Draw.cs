c73b56396dbf06ce4cea68d8fa2399a0e238485f
CamDHall <camerondylan.h@gmail.com>
2016-12-12 11:03:32 -0500

    Backup

--- ScribbleNOT/Assets/Script/Draw.cs
+++ ScribbleNOT/Assets/Script/Draw.cs
@@ -4,12 +4,28 @@ using System.Collections;
 public class Draw : MonoBehaviour {
 
     public Mesh redMesh;
+    public Mesh blueMesh;
+
+    public Material redMat;
+    public Material blueMat;
 
     void Start () {
-	
+        RedDraw();
 	}
 	
 	void Update () {
-	
-	}
+        Graphics.DrawMesh(redMesh, Vector3.zero, Quaternion.identity, redMat, 0);
+    }
+
+    void RedDraw()
+    {
+        Graphics.DrawMesh(redMesh, Vector3.zero, Quaternion.identity, redMat, 0);
+    }
+
+    void BlueDraw()
+    {
+
+    }
+
+
 }
