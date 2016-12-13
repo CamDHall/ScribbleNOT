a705baba5ac8f45c80f39c8222b42c86f4d1d417
Sam Leiman <srl447@nyu.edu>
2016-12-13 12:11:33 -0500

    Scoring

---
+++ ScribbleNOT/Assets/Script/TotalScore.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TotalScore : MonoBehaviour {
+    public static int score;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(score);
+	
+	}
+}
