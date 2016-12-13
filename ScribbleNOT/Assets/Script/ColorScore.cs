8e17178b27f05a67ee559f6347bb5593baa19016
CamDHall <camerondylan.h@gmail.com>
2016-12-13 11:34:48 -0500

    Color Update

---
+++ ScribbleNOT/Assets/Script/ColorScore.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorScore : MonoBehaviour {
+
+    public Color blue = new Color(0, 0, 255, 255);
+    public Color red = new Color(255, 0, 0, 255);
+
+	void Start () {
+
+	}
+	
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D (Collider2D coll)
+    {
+        if (coll.gameObject.tag == "RedPen")
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().material.color = red; 
+        }
+
+        if (coll.gameObject.tag == "BluePen")
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().material.color = blue;
+        }
+    }
+}
--- ScribbleNOT/Assets/Script/placeSquare.cs
+++ ScribbleNOT/Assets/Script/placeSquare.cs
@@ -8,7 +8,8 @@ public class placeSquare : MonoBehaviour {
     int count = 0;
     int count2 = 0;
     Vector3 placement = new Vector3 (-9, 6, 0);
-    // Use this for initialization
+
+
     void Awake ()
     {
         for (int count2 = 0; count2 < 101; count2++)
@@ -23,21 +24,8 @@ public class placeSquare : MonoBehaviour {
         }
     }
 	
-	// Update is called once per frame
 	void Update ()
     { 
-        /*if (count < 180)
-        {
-            GameObject newSquare = Instantiate(square) as GameObject;
-            placement = placement + new Vector3(.1f, 0, 0);
-            newSquare.transform.position = placement;
-            count++;
-        }
-        if (count >= 180 && count2 < 100)
-        {
-            placement = placement + new Vector3(-18, -.1f, 0);
-            count = 0;
-            count2++;
-        }*/
+
     }
 }
