0f4d8612fe415c0d7d8b048c932a519cd9b0fc26
Sam Leiman <srl447@nyu.edu>
2016-12-13 11:16:18 -0500

    Movement Scripts

---
+++ ScribbleNOT/Assets/Script/MoveBlue.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBlue : MonoBehaviour {
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey("d"))
+        {
+            transform.Translate(2*Vector3.right * Time.deltaTime);
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.Translate(2*-Vector3.right * Time.deltaTime);
+        }
+        if (Input.GetKey("w"))
+        {
+            transform.Translate(2*Vector3.up * Time.deltaTime);
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.Translate(2*-Vector3.up * Time.deltaTime);
+        }
+
+    }
+}
---
+++ ScribbleNOT/Assets/Script/MoveRed.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveRed : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey("right"))
+        {
+            transform.Translate(2 * Vector3.right * Time.deltaTime);
+        }
+        if (Input.GetKey("left"))
+        {
+            transform.Translate(2 * -Vector3.right * Time.deltaTime);
+        }
+        if (Input.GetKey("up"))
+        {
+            transform.Translate(2 * Vector3.up * Time.deltaTime);
+        }
+        if (Input.GetKey("down"))
+        {
+            transform.Translate(2 * -Vector3.up * Time.deltaTime);
+        }
+    }
+}
