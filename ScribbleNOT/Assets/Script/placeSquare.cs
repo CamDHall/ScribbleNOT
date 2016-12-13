56c0c220d59d7dd63ee24536c056d30470747fa3
Sam Leiman <srl447@nyu.edu>
2016-12-13 10:38:13 -0500

    Code for Canvas

---
+++ ScribbleNOT/Assets/Script/placeSquare.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class placeSquare : MonoBehaviour {
+    float xPlace = -9f;
+    float yPlace = 0f;
+    public GameObject square;
+    int count = 0;
+    int count2 = 0;
+    Vector3 placement = new Vector3 (-9, 6, 0);
+    // Use this for initialization
+    void Awake ()
+    {
+        for (int count2 = 0; count2 < 101; count2++)
+        {
+            for (int count = 0; count < 180; count++)
+            {
+                GameObject newSquare = Instantiate(square) as GameObject;
+                placement = placement + new Vector3(.1f, 0, 0);
+                newSquare.transform.position = placement;
+            }
+            placement = placement + new Vector3(-18, -.1f, 0);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    { 
+        /*if (count < 180)
+        {
+            GameObject newSquare = Instantiate(square) as GameObject;
+            placement = placement + new Vector3(.1f, 0, 0);
+            newSquare.transform.position = placement;
+            count++;
+        }
+        if (count >= 180 && count2 < 100)
+        {
+            placement = placement + new Vector3(-18, -.1f, 0);
+            count = 0;
+            count2++;
+        }*/
+    }
+}
