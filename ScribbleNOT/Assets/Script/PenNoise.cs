a02418891383b96210c08777a8dd61657fe74ecc
Sam Leiman <srl447@nyu.edu>
2016-12-14 11:05:00 -0500

    noicse

---
+++ ScribbleNOT/Assets/Script/PenNoise.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PenNoise : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKeyDown("left shift"))
+        {
+            AudioSource audio = GetComponent<AudioSource>();
+            audio.Play();
+        }
+        if (Input.GetKeyUp("left shift"))
+        {
+            AudioSource audio = GetComponent<AudioSource>();
+            audio.Stop();
+        }
+    }
+}
